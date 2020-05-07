1fb30c8e3fde909f2721533ab1586274e13c6a8c
Jason Junio <jjjunio@gmail.com>
2020-05-07 21:02:48 +0000

    created breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,24 @@
 const request = require('request');
-const breedName = process.argv[2];
+// const breedName = process.argv[2];
 
 
-const fetchBreedDescription = function (breedName, callback) {
+
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   // console.log('error:', error); // Print the error if one occurred
   // console.log(response); // Print the response status code if a response was received
-    if(error) {
+    if (error) {
       callback(error);
     } else {
       const data = JSON.parse(body);
-    if(data.length === 0) {
-      callback("Breed Not Found!");
-    } else {
-      callback(null, data[0]['description']);
-    }
+      if (data.length === 0) {
+        callback("Breed Not Found!");
+      } else {
+        callback(null, data[0]['description']);
+      }
     }
   // console.log('body:', body); // Print the HTML for the Google homepage.
-});
+  });
 };
 
 module.exports = { fetchBreedDescription };
